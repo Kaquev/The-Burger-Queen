836195fc6a6ad8ab36820d9285dcbf934740fb61
karla aqueveque <aquevequekarla@gmail.com>
2023-12-14 18:12:15 -0300

    maqueta login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass'],
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginOn!: FormGroup;
