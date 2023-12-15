f4f26fd375427b4e519ec081a5ba3bc749abfda4
karla aqueveque <aquevequekarla@gmail.com>
2023-12-15 17:02:49 -0300

    maqueta order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'order-summary', component: OrderSummaryComponent },
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   // email: string = '';
   // password: string = '';
 
-  constructor(private readonly fb: FormBuilder) {}
+  constructor(private readonly fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.loginOn = this.initForm(); // Inicializa el formulario
@@ -19,6 +20,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
     console.log('Error de autenticación');
+    this.router.navigate(['/order-summary']);
   }
 
   initForm(): FormGroup {
